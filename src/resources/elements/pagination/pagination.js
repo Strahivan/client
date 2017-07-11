765a887e003d003423d8db96bdcffb7519df5d01
AFM Sayem <afm.sayem@gmail.com>
2017-07-11 15:23:31 +0800

    feat(pagination): restructure pagination with link based navigation for search engine crawlers

--- src/resources/elements/pagination/pagination.js
+++ src/resources/elements/pagination/pagination.js
@@ -1,15 +1,39 @@
-import {bindable} from 'aurelia-framework';
-import animateScrollTo from 'animated-scroll-to';
+import {bindable, computedFrom} from 'aurelia-framework';
+import {buildQueryString} from 'aurelia-path';
 
 export class Pagination {
   @bindable total;
-  @bindable page;
-  @bindable fetcher;
+  @bindable path;
+  @bindable params;
+  @bindable size;
 
-  paginate(pageNumber) {
-    this.page.number = pageNumber;
-    this.fetcher();
-    animateScrollTo(0);
+  attached() {
+    this.nextParams = Object.assign({}, this.params);
+    this.nextParams.page = Number(this.nextParams.page) + 1;
+    this.prevParams = Object.assign({}, this.params);
+    this.prevParams.page = Number(this.prevParams.page) - 1;
+  }
+
+  @computedFrom('total')
+  get prevUrl() {
+    if (this.prevParams && this.prevParams.page < 0) {
+      this.prevElem.setAttribute('rel', 'nofollow');
+      const newParams = Object.assign({}, this.prevParams);
+      newParams.page = this.prevParams.page + 1;
+      return `${this.path}?${buildQueryString(newParams)}`;
+    }
+    return `${this.path}?${buildQueryString(this.prevParams)}`;
+  }
+
+  @computedFrom('total')
+  get nextUrl() {
+    if (this.nextParams && this.total <= (this.nextParams.page * this.size)) {
+      this.nextElem.setAttribute('rel', 'nofollow');
+      const newParams = Object.assign({}, this.nextParams);
+      newParams.page = this.nextParams.page - 1;
+      return `${this.path}?${buildQueryString(newParams)}`;
+    }
+    return `${this.path}?${buildQueryString(this.nextParams)}`;
   }
 }
 
