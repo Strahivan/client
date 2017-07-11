bce8cf42f7d34964438df5d7b10fef7cf61f0074
AFM Sayem <afm.sayem@gmail.com>
2017-07-11 19:57:16 +0800

    feat(shops): implement url based filters in requests

--- src/views/user/shops/requests/list.js
+++ src/views/user/shops/requests/list.js
@@ -1,8 +1,10 @@
 import {inject} from 'aurelia-framework';
 import {Api} from '~/services/api';
 import {constants} from '~/services/constants';
+import {activationStrategy} from 'aurelia-router';
+import {Router} from 'aurelia-router';
 
-@inject(Api)
+@inject(Api, Router)
 export class ShopRequestListVM {
   requests = {
     params: {
@@ -18,9 +20,32 @@ export class ShopRequestListVM {
   }
   shop = {};
   products = {};
-  constructor(api) {
+
+  constructor(api, router) {
     this.api = api;
+    this.router = router;
     this.statuses = constants.requestStatus;
+  }
+
+  determineActivationStrategy() {
+    return activationStrategy.replace;
+  }
+
+  reload(params) {
+    const query = {
+      status: params.filter['status:eq'],
+      created_at_gt: params.filter['created_at:gt'],
+      created_at_lt: params.filter['createtd_at:lt'],
+      product: params.filter['product_id:eq'],
+      page: params.page.number
+    };
+
+    this.router.navigateToRoute('userRequestList', query);
+  }
+
+  resetPageAndFetch() {
+    this.requests.params.page.number = 0;
+    this.reload(this.requests.params);
   }
 
   getOrders() {
@@ -32,13 +57,23 @@ export class ShopRequestListVM {
       .catch(err => console.log(err));
   }
 
-  activate(params) {
+  activate(params, config, instruction) {
+    this.path = window.location.pathname.substring(1);
+    this.query = Object.assign({}, instruction.queryParams);
+
+    this.query.page = this.query.page || 0;
+    this.requests.params.filter['status:eq'] = this.query.status || 'pending';
+    this.requests.params.filter['created_at:gt'] = this.query.created_at_gt && (new Date(this.query.created_at_gt)).toISOString();
+    this.requests.params.filter['created_at:lt'] = this.query.created_at_lt && (new Date(this.query.created_at_lt)).toISOString();
+    this.requests.params.filter['product_id:eq'] = this.query.product && Number(this.query.product);
+
     this.params = params;
+
     this.getOrders();
     this.api.fetch(`me/shops/${params.shop_id}`)
       .then(shop => this.shop.data = shop)
       .catch(err => console.log(err));
-    this.api.fetch(`me/shops/${params.shop_id}/products`)
+    this.api.fetch(`me/shops/${params.shop_id}/products`, {page: {size: 1000}})
       .then(products => this.products.data = products.results)
       .catch(err => console.log(err));
   }
