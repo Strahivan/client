129bf1f4815dcab7f1f9e694cca28f3379329a31
AFM Sayem <afm.sayem@gmail.com>
2017-07-25 21:43:42 +0800

    feat(price estimator): put custom order link in price estimator widget

--- src/views/custom-order/price-estimator.js
+++ src/views/custom-order/price-estimator.js
@@ -3,15 +3,22 @@ import {DialogController} from 'aurelia-dialog';
 import {UserStore} from '~/stores/user';
 import {CountryStore} from '~/stores/country';
 import {PriceService} from '~/services/price';
+import {Router} from 'aurelia-router';
 
-@inject(DialogController)
+@inject(DialogController, Router)
 export class PriceEstimatorDialog {
 
-  constructor(dialog) {
+  constructor(dialog, router) {
     this.dialog = dialog;
     this.user = UserStore.user;
+    this.router = router;
     this.priceService = PriceService;
     this.countries = CountryStore.countries;
+  }
+
+  goToCustomPurchase() {
+    this.router.navigateToRoute('custom-order');
+    this.dialog.ok();
   }
 
   calculate() {
