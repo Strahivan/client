8dcc4bc1c2e71d8f63df5951141b06012e153a49
AFM Sayem <afm.sayem@gmail.com>
2017-07-27 14:22:33 +0800

    chore(bourbon/neat): do not add custom path for sass processing

--- aurelia_project/tasks/process-css.js
+++ aurelia_project/tasks/process-css.js
@@ -2,13 +2,11 @@ import gulp from 'gulp';
 import sass from 'gulp-sass';
 import project from '../aurelia.json';
 import {build} from 'aurelia-cli';
-import bourbon from 'bourbon';
-import neat from 'bourbon-neat';
+import * as sourcemaps from 'gulp-sourcemaps';
 
 export default function processCSS() {
   return gulp.src(project.cssProcessor.source)
-    .pipe(sass({
-      includePaths: [].concat(bourbon.includePaths, neat.includePaths)
-    }).on('error', sass.logError))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.init())
     .pipe(build.bundle());
 }
