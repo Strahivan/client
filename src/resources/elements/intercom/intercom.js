48b295153e102835be07f2a73b2675177590a9d5
AFM Sayem <afm.sayem@gmail.com>
2017-07-28 16:55:37 +0800

    feat(intercom): identify loggedin users

--- src/resources/elements/intercom/intercom.js
+++ src/resources/elements/intercom/intercom.js
@@ -1,37 +1,50 @@
+import {inject} from 'aurelia-framework';
+import {UserStore} from '~/stores/user';
+
+@inject(UserStore)
 export class Intercom {
+
+  constructor(user) {
+    this.user = user.user;
+  }
+
   intercom() {
-    var w = window;
-    var ic = w.Intercom;
-    if (typeof ic === "function") {
-      ic('reattach_activator');
-      ic('update');
+    const win = window;
+    const intercom = win.Intercom;
+    if (typeof intercom === 'function') {
+      intercom('reattach_activator');
+      intercom('update', {
+        email: this.user && this.user.email,
+        user_id: this.user && this.user.id,
+        name: this.user && this.user.name
+      });
     } else {
-      var d = document;
-      var i = function() {
+      const doc = document;
+      const i = function() {
         i.c(arguments)
       };
       i.q = [];
       i.c = function(args) {
-        i.q.push(args)
+        i.q.push(args);
       };
-      w.Intercom = i;
+      win.Intercom = i;
 
-      function l() {
-        var s = d.createElement('script');
-        s.type = 'text/javascript';
-        s.async = true;
-        s.src = 'https://widget.intercom.io/widget/bszrr242';
-        var x = d.getElementsByTagName('script')[0];
-        x.parentNode.insertBefore(s, x);
-      }
-      l();
+      const tag = doc.createElement('script');
+      tag.type = 'text/javascript';
+      tag.async = true;
+      tag.src = 'https://widget.intercom.io/widget/bszrr242';
+      const firstScriptTag = doc.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
     }
   }
 
   attached() {
     this.intercom();
     window.Intercom('boot', {
-      app_id: 'bszrr242'
+      app_id: 'bszrr242',
+      email: this.user && this.user.email,
+      user_id: this.user && this.user.id,
+      name: this.user && this.user.name
     });
   }
 }
