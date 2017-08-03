591ed6ac66caa7cc57d446c5088bb018c186f9d4
AFM Sayem <afm.sayem@gmail.com>
2017-08-03 15:37:06 +0800

    feat(collection): enable pagination on collections

--- src/views/collection/collection.js
+++ src/views/collection/collection.js
@@ -1,13 +1,22 @@
 import {inject} from 'aurelia-framework';
 import {Api} from '~/services/api';
 import {PriceService} from '~/services/price';
+import {activationStrategy} from 'aurelia-router';
 
 @inject(Api)
 export class CollectionView {
-  collectionproducts = {};
+  collectionproducts = {
+    params: {
+      page: {
+        size: 24
+      },
+      include: ['product', 'product.source']
+    }
+  };
   collection = {
     page: {
-      size: 20
+      size: 20,
+      number: 0
     }
   };
 
@@ -15,15 +24,26 @@ export class CollectionView {
     this.api = api;
   }
 
-  activate(param) {
+  determineActivationStrategy() {
+    return activationStrategy.replace;
+  }
+
+  activate(param, config, instruction) {
+    this.query = Object.assign({}, instruction.queryParams);
+    this.params = param;
+
+    this.collectionproducts.params.page.number = param.page && Number(param.page) || 0;
+    this.query.page = this.query.page && Number(this.query.page) || 0;
+
     this.api
       .fetch(`collections/${param.collection_id}`)
       .then(collection => this.collection.data = collection)
-      .catch(err => this.collection.error = err);
+      .catch(err => console.log(err));
 
     this.api
-      .fetch(`collections/${param.collection_id}/collectionproducts`, {include: ['product', 'product.source'], page: {size: 24}})
+      .fetch(`collections/${param.collection_id}/collectionproducts`, this.collectionproducts.params)
       .then(response => {
+        this.collectionproducts.total = response.total;
         this.collectionproducts.data = response.results.map(cp => {
           cp.product.price = PriceService.calculatePrice(cp.product);
           return cp;
