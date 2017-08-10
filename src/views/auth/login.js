f6e7dad0cc2437058c7b806b2979a2c3f37f3f04
AFM Sayem <afm.sayem@gmail.com>
2017-08-10 18:20:42 +0800

    fix(login): only show one error message for wrong login

--- src/views/auth/login.js
+++ src/views/auth/login.js
@@ -2,24 +2,32 @@ import {inject} from 'aurelia-framework';
 import {Api} from '~/services/api';
 import {AuthService} from 'aurelia-auth';
 import {UserStore} from '~/stores/user';
+import {ErrorReporting} from '~/services/error-reporting';
 
-@inject(AuthService, Api, UserStore)
+@inject(AuthService, Api, UserStore, ErrorReporting)
 export class Login {
   errors = [];
-  constructor(auth, api, userStore) {
+  constructor(auth, api, userStore, errorReporting) {
     this.auth = auth;
     this.api = api;
     this.userStore = userStore;
+    this.errorReporting = errorReporting;
+    this.state = {
+      error: {}
+    };
   }
 
   login() {
+    this.state.error.wrongLogin = null;
     return this.auth.login(this.email, this.password)
       .then(response => this.api.fetch('me', {include: ['country', 'shops']}))
       .then(user => this.userStore.save(user))
       .catch(err => {
         if (err.status === 422) {
-          this.errors.push('Wrong username or password');
+          this.state.error.wrongLogin = 'Wrong username or password';
+          return;
         }
+        this.errorReporting.report(new Error(err.message));
       });
   }
 }
