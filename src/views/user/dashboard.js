f622dd2485ec371ad01514075ecc95e405c4aadf
AFM Sayem <afm.sayem@gmail.com>
2017-08-10 19:21:16 +0800

    refactor(dashboard): remove unused dependency

--- src/views/user/dashboard.js
+++ src/views/user/dashboard.js
@@ -1,12 +1,11 @@
 import {inject} from 'aurelia-framework';
-import {Router} from 'aurelia-router';
 import {Api} from '~/services/api';
 import {constants} from '~/services/constants';
 import {UserStore} from '~/stores/user';
 import {AuthService} from 'aurelia-auth';
 import {ErrorReporting} from '~/services/error-reporting';
 
-@inject(Api, Router, UserStore, AuthService, ErrorReporting)
+@inject(Api, UserStore, AuthService, ErrorReporting)
 export class DashboardView {
   requests = {
     params: {
@@ -20,9 +19,8 @@ export class DashboardView {
   };
   shops = {};
 
-  constructor(api, router, userStore, auth, errorReporting) {
+  constructor(api, userStore, auth, errorReporting) {
     this.api = api;
-    this.router = router;
     this.user = userStore.user;
     this.auth = auth;
     this.errorReporting = errorReporting;
