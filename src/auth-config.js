46c4030e507710ba7d50ca8adff281d76d77efbc
AFM Sayem <afm.sayem@gmail.com>
2017-08-16 17:15:21 +0800

    chore(facebook): add facebook authentication configuration

--- src/auth-config.js
+++ src/auth-config.js
@@ -1,11 +1,31 @@
+import environment from '~/environment';
+
 const configForDevelopment = {
   responseTokenProp: 'token',
-  baseUrl: 'http://localhost:3000/',
+  baseUrl: environment.base,
   logoutRedirect: '/',
   loginRoute: '/auth',
   signupRoute: '/auth/signup',
   profileUrl: '/me',
-  tokenPrefix: ''
+  tokenPrefix: '',
+  providers: {
+    facebook: {
+      name: 'facebook',
+      url: '/auth/facebook',
+      authorizationEndpoint: 'https://www.facebook.com/v2.10/dialog/oauth',
+      redirectUri: window.location.origin + '/' || window.location.protocol + '//' + window.location.host + '/',
+      scope: ['email'],
+      scopeDelimiter: ',',
+      nonce: function() {
+        return Math.random();
+      },
+      requiredUrlParams: ['nonce', 'display', 'scope'],
+      display: 'popup',
+      type: '2.0',
+      clientId: '116846465638888',
+      popupOptions: { width: 580, height: 400 }
+    }
+  }
 };
 
 const configForProduction = {
