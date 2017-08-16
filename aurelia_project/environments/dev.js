fda124cf8772922f826f699a5c65e1fb9555864d
AFM Sayem <afm.sayem@gmail.com>
2017-08-16 17:14:38 +0800

    feat(facebook): add facebook app id to environments

--- aurelia_project/environments/dev.js
+++ aurelia_project/environments/dev.js
@@ -1,8 +1,9 @@
 export default {
   debug: true,
   testing: true,
-  base: 'http://192.168.1.156:3000/',
+  base: 'http://localhost:3000/',
   stripe_key: 'pk_test_y6c6G7emK6M5t1tfURhHiWQz',
+  facebook: '116846465638888',
   ga_tracking_id: 'UA-101140118-1',
   ga_tracking_enabled: false,
   adwordtracking: false,
--- aurelia_project/environments/prod.js
+++ aurelia_project/environments/prod.js
@@ -3,6 +3,7 @@ export default {
   testing: false,
   base: 'https://api.novelship.com/',
   stripe_key: 'pk_live_ZSoVkfqE1IhCf6OJP9HtScJ4',
+  facebook: '2024958714406360',
   ga_tracking_id: 'UA-101140118-1',
   ga_tracking_enabled: true,
   adwordtracking: true,
