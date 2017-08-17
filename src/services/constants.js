8d9cbed791467da8da5d94dba78013cedae2dd05
AFM Sayem <afm.sayem@gmail.com>
2017-08-17 11:58:55 +0800

    feat(constants): add shipping to status constants

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,6 +1,6 @@
 export const constants = {
   requestStatus: [
-    'pending', 'confirmed', 'processing', 'ready', 'completed', 'verify', 'approval', 'canceled', 'delivering', 'failed', 'rejected'
+    'pending', 'confirmed', 'processing', 'ready', 'completed', 'verify', 'approval', 'canceled', 'delivering', 'failed', 'rejected', 'shipping'
   ],
   deliveryOptions: [
     'pickup', 'courier'
