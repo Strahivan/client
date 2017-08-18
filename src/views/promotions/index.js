409b2f3049c20ec5c72597decfd0e6b014510abd
AFM Sayem <afm.sayem@gmail.com>
2017-08-18 18:47:35 +0800

    chore(fila): rename route of giveaway

--- src/views/promotions/index.js
+++ src/views/promotions/index.js
@@ -2,7 +2,7 @@ export class PromotionsRouter {
   configureRouter(config, router) {
     config.map([
       {route: '', name: 'all', moduleId: './fila-giveaway'},
-      {route: 'fila-giveaway', name: 'fila-giveaway', auth: true, moduleId: './fila-giveaway'}
+      {route: 'novelship-giveaway', name: 'fila-giveaway', auth: true, moduleId: './fila-giveaway'}
     ]);
   }
 }
