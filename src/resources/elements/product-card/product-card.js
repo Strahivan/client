8979b346e11f3084fe52f45b886962575783a93a
AFM Sayem <afm.sayem@gmail.com>
2017-08-28 13:46:19 +0800

    feat(product card): product card to calculate product prices

--- src/resources/elements/product-card/product-card.js
+++ src/resources/elements/product-card/product-card.js
@@ -1,11 +1,16 @@
 import {inject, bindable} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
+import {PriceService} from '~/services/price';
 
 @inject(Router)
 export class ProductCard {
   @bindable product;
   constructor(router) {
     this.router = router;
+  }
+
+  attached() {
+    this.product.price = PriceService.calculatePrice(this.product);
   }
 
   showImage(index) {
