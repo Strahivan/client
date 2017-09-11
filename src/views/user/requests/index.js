b0521bd738338d698f13b878b33aa13bdc47a7c9
AFM Sayem <afm.sayem@gmail.com>
2017-09-11 20:23:16 +0800

    feat(acknowledge): add custom order acknowledge page

--- src/views/user/requests/index.js
+++ src/views/user/requests/index.js
@@ -4,7 +4,9 @@ export class UserRequestsRouter {
   configureRouter(config, router) {
     config.map([
       {route: '', name: 'userRequestList', moduleId: './list'},
-      {route: ':request_id', name: 'userRequest', moduleId: './request'}
+      {route: ':request_id', name: 'userRequest', moduleId: './request'},
+      {route: ':request_id/acknowledge', name: 'acknowledge', moduleId: './acknowledge'},
+      {route: ':request_id/custom-checkout', name: 'customCheckout', moduleId: './custom-checkout'}
     ]);
   }
 }
