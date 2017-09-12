1faee09d2ce1916ffbada79ab94614aac1e48c14
AFM Sayem <afm.sayem@gmail.com>
2017-09-12 16:48:12 +0800

    feat(notification): add error handling notification

--- src/services/notification.js
+++ src/services/notification.js
@@ -1,4 +1,11 @@
 import Humane from 'humane-js';
+
 export function notify() {
-  return Humane.create({timeout: 1500, baseCls: 'humane-flatty'});
+  return Humane.create({timeout: 3500, baseCls: 'humane-flatty'});
+}
+
+export function ajaxErrorHandler(err) {
+  if (err instanceof Response) {
+    err.json().then((error) => notify().log(error.message))
+  }
 }
