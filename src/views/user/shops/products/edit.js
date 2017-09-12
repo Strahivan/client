2ae6ba541db8e41a4b4ff05dc5c3d398c684bddb
AFM Sayem <afm.sayem@gmail.com>
2017-09-12 17:12:37 +0800

    feat(edit product): implement remove from collection

--- src/views/user/shops/products/edit.js
+++ src/views/user/shops/products/edit.js
@@ -1,6 +1,6 @@
 import {inject} from 'aurelia-framework';
 import {Api} from '~/services/api';
-import {notify} from '~/services/notification';
+import {notify, ajaxErrorHandler} from '~/services/notification';
 import {utilities} from '~/services/utilities';
 import {PriceService} from '~/services/price';
 
@@ -14,32 +14,37 @@ export class EditProductView {
   activate(params) {
     this.params = params;
     this.api
-      .fetch(`products/${params.product_id}`)
+      .fetch(`products/${params.product_id}`, {include: ['collections']})
       .then(product => {
         this.product = product;
         this.newProduct = JSON.parse(JSON.stringify(this.product));
       })
-      .catch(err => {
-        console.log(err);
-      });
+      .catch(ajaxErrorHandler);
 
     this.api
       .fetch('collections')
       .then(data => {
         this.collections = data.results;
       })
-      .catch(err => {
-        notify.log(err.message);
-      });
+      .catch(ajaxErrorHandler);
   }
 
   updateCollection(collectionId) {
     this.api
       .create(`collections/${collectionId}/collectionproducts`, { product_id: this.product.id })
-      .then(success => notify().log('Successfully Updated'))
-      .catch(err => {
-        err.json().then(res => notify().log(res.err));
-      });
+      .then(success => notify().log('Added to new collection'))
+      .catch(ajaxErrorHandler);
+  }
+
+  removeFromCollection(collectionId) {
+    this.api
+      .fetch(`collections/${collectionId}/collectionproducts`, {filter: {'product_id:eq': this.product.id}})
+      .then(collectionproduct => this.api.remove(`collections/${collectionId}/collectionproducts/${collectionproduct.results[0].id}`))
+      .then(success => {
+        notify().log('Successfully removed');
+        this.newProduct.collections = this.newProduct.collections.filter(collection => collection.id !== collectionId);
+      })
+      .catch(ajaxErrorHandler);
   }
 
   getPrice() {
@@ -48,11 +53,10 @@ export class EditProductView {
 
   edit() {
     const fragment = utilities.filterUntouchedProperties(this.product, this.newProduct);
-    console.log(fragment);
     this.api
       .edit(`products/${this.params.product_id}`, fragment)
       .then(success => notify().log('Successfully Updated'))
-      .catch(err => console.log(err));
+      .catch(ajaxErrorHandler);
   }
 
   swap(from, to) {
