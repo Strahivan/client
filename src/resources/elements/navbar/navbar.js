777be5e2cea9975f817c9f268eee8f96d7c063d3
AFM Sayem <afm.sayem@gmail.com>
2017-09-18 19:06:31 +0800

    feat(navbar): make whole are clickable

--- src/resources/elements/navbar/navbar.js
+++ src/resources/elements/navbar/navbar.js
@@ -2,18 +2,26 @@ import {bindable, inject} from 'aurelia-framework';
 import {UserStore} from '~/stores/user';
 import {AuthService} from 'aurelia-auth';
 import {Api} from '~/services/api';
+import {Router} from 'aurelia-router';
+import {utilities} from '~/services/utilities';
 
-@inject(UserStore, AuthService, Api)
+@inject(UserStore, AuthService, Api, Router)
 export class Navbar {
   @bindable router;
-  constructor(userStore, auth, api) {
+  constructor(userStore, auth, api, approuter) {
     this.auth = auth;
     this.userStore = userStore;
+    this.approuter = approuter;
     api
-      .fetch('categories')
+      .fetch('categories', {sort: 'id', include: ['subcategories'], filter: {'parent_id:isNull': true}})
       .then(categories => {
         this.categories = categories.results;
       });
+  }
+
+  goToCategory(category) {
+    utilities.closeMobileMenu();
+    this.approuter.navigate(`/filter?category=${category.id}`);
   }
 
   showMenu() {
