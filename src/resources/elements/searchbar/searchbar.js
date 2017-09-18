6b94546c806a3398d2db4cde0bf42bae67717a6c
AFM Sayem <afm.sayem@gmail.com>
2017-09-18 14:11:31 +0800

    fix(search): close burger menu when searched from mobile

--- src/resources/elements/searchbar/searchbar.js
+++ src/resources/elements/searchbar/searchbar.js
@@ -10,14 +10,27 @@ export class Searchbar {
   }
 
   showResults() {
+    // close mobile menu if the menu is active
+    const menu = document.getElementsByClassName('navbar-menu')[0];
+    const burger = document.getElementsByClassName('navbar-burger')[0];
+    if (menu.classList.contains('is-active')) {
+      menu.classList.remove('is-active');
+    }
+    if (burger.classList.contains('is-active')) {
+      burger.classList.remove('is-active');
+    }
+
     // if you are already in filter
     // then publish an event with query
     // the filter page will read that query, update it's parameter value
     // and refetch products
+    //
     if (this.router.currentInstruction.config.name === 'filter') {
       this.ea.publish('filter__search', this.query);
       return;
     }
+
+
     this.router.navigateToRoute('filter', {search: this.query});
   }
 }
