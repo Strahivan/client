d095fe1ba0fcfc1b4db809d17856d448e190c63d
AFM Sayem <afm.sayem@gmail.com>
2017-09-18 13:02:51 +0800

    feat(navbar): replace existing navbar with hamburger menu

--- src/resources/elements/navbar/navbar.js
+++ src/resources/elements/navbar/navbar.js
@@ -1,12 +1,24 @@
 import {bindable, inject} from 'aurelia-framework';
 import {UserStore} from '~/stores/user';
 import {AuthService} from 'aurelia-auth';
+import {Api} from '~/services/api';
 
-@inject(UserStore, AuthService)
+@inject(UserStore, AuthService, Api)
 export class Navbar {
   @bindable router;
-  constructor(userStore, auth) {
+  constructor(userStore, auth, api) {
     this.auth = auth;
     this.userStore = userStore;
+    api
+      .fetch('categories')
+      .then(categories => {
+        this.categories = categories.results;
+      });
   }
+
+  showMenu() {
+    this.menuButton.classList.toggle('is-active');
+    this.menu.classList.toggle('is-active');
+  }
+
 }
