340b2100a7545d5d2a62ef84bc9d0cd8207c54ee
AFM Sayem <afm.sayem@gmail.com>
2017-10-04 18:32:07 +0800

    feat(intercom): push user info to intercom post login

--- src/resources/elements/intercom/intercom.js
+++ src/resources/elements/intercom/intercom.js
@@ -10,32 +10,22 @@ export class Intercom {
 
   intercom() {
     const win = window;
-    const intercom = win.Intercom;
-    if (typeof intercom === 'function') {
-      intercom('reattach_activator');
-      intercom('update', {
-        email: this.userStore.user && this.userStore.user.email,
-        user_id: this.userStore.user && this.userStore.user.id,
-        name: this.userStore.user && this.userStore.user.name
-      });
-    } else {
-      const doc = document;
-      const i = function() {
-        i.c(arguments);
-      };
-      i.q = [];
-      i.c = function(args) {
-        i.q.push(args);
-      };
-      win.Intercom = i;
+    const doc = document;
+    const i = function() {
+      i.c(arguments);
+    };
+    i.q = [];
+    i.c = function(args) {
+      i.q.push(args);
+    };
+    win.Intercom = i;
 
-      const tag = doc.createElement('script');
-      tag.type = 'text/javascript';
-      tag.async = true;
-      tag.src = 'https://widget.intercom.io/widget/bszrr242';
-      const firstScriptTag = doc.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-    }
+    const tag = doc.createElement('script');
+    tag.type = 'text/javascript';
+    tag.async = true;
+    tag.src = 'https://widget.intercom.io/widget/bszrr242';
+    const firstScriptTag = doc.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
   }
 
   attached() {
--- src/stores/user.js
+++ src/stores/user.js
@@ -6,6 +6,15 @@ export class UserStore {
 
   set user(val) {
     this._user = val;
+    const intercom = window.Intercom;
+    if (typeof intercom === 'function') {
+      intercom('reattach_activator');
+      intercom('update', {
+        email: this.userStore.user && this.userStore.user.email,
+        user_id: this.userStore.user && this.userStore.user.id,
+        name: this.userStore.user && this.userStore.user.name
+      });
+    }
   }
 
   clear() {
