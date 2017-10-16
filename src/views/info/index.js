e262c9585f795d4778070ec81c325c2e5790df00
AFM Sayem <afm.sayem@gmail.com>
2017-10-16 18:23:01 +0800

    feat(about): add scaffolding for about us(WIP)

--- src/views/info/index.js
+++ src/views/info/index.js
@@ -2,7 +2,8 @@ export class FooterRouter {
   configureRouter(config, router) {
     config.map([
       {route: 'contact', moduleId: './contact', title: 'Contact'},
-      {route: 'refunds', moduleId: './refunds', title: 'Refunds'}
+      {route: 'refunds', moduleId: './refunds', title: 'Refunds'},
+      {route: 'about', moduleId: './about', title: 'About'}
     ]);
   }
 }
