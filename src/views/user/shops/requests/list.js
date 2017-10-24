792a5b0eafabc9fe13656f897bd9f6931f76fb5d
AFM Sayem <afm.sayem@gmail.com>
2017-10-24 12:47:33 +0800

    feat(product search): allow finding orders by product

--- src/views/user/shops/requests/list.js
+++ src/views/user/shops/requests/list.js
@@ -1,7 +1,6 @@
 import {inject} from 'aurelia-framework';
 import {Api} from '~/services/api';
 import {constants} from '~/services/constants';
-import {activationStrategy} from 'aurelia-router';
 import {Router} from 'aurelia-router';
 import {CountryStore} from '~/stores/country';
 
@@ -19,6 +18,7 @@ export class ShopRequestListVM {
   }
   shop = {};
   products = {};
+  state = {};
 
   constructor(api, router, countryStore) {
     this.api = api;
@@ -27,29 +27,13 @@ export class ShopRequestListVM {
     this.countryStore = countryStore;
   }
 
-  determineActivationStrategy() {
-    return activationStrategy.replace;
-  }
-
-  reload(params) {
-    const query = {
-      status: params.filter['status:eq'],
-      email: params.filter['customer.email:eq'],
-      source: params.filter['source_id:eq'],
-      product: params.filter['product_id:eq'],
-      page: params.page.number
-    };
-
-    this.router.navigateToRoute('userRequestList', query);
-  }
-
   resetPageAndFetch() {
     this.requests.params.page.number = 0;
-    this.reload(this.requests.params);
+    this.getOrders();
   }
 
   getOrders() {
-    this.api.fetch(`me/shops/${this.params.shop_id}/requests`, this.requests.params)
+    this.api.fetch('requests', this.requests.params)
       .then(requests => {
         this.requests.data = requests.results;
         this.requests.total = requests.total;
@@ -57,16 +41,8 @@ export class ShopRequestListVM {
       .catch(err => console.log(err));
   }
 
-  activate(params, config, instruction) {
-    this.path = window.location.pathname.substring(1);
-    this.query = Object.assign({}, instruction.queryParams);
-
-    this.query.page = this.query.page || 0;
-    this.requests.params.filter['status:eq'] = this.query.status;
-    this.requests.params.filter['customer.email:eq'] = this.query.email;
-    this.requests.params.filter['source_id:eq'] = this.query.source && Number(this.query.source);
-    this.requests.params.filter['product_id:eq'] = this.query.product && Number(this.query.product);
-    this.requests.params.page.number = (this.query.page && Number(this.query.page)) || 0;
+  activate(params) {
+    this.requests.params.filter['shop_id:eq'] = params.shop_id && Number(params.shop_id);
 
     this.params = params;
 
