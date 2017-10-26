fffa65846fb82c4505877c09b0d5ea2cbea25350
AFM Sayem <afm.sayem@gmail.com>
2017-10-26 18:52:21 +0800

    feat(tracking): add tracking to batches and set orders to shipping

--- src/views/user/shops/batches/list.js
+++ src/views/user/shops/batches/list.js
@@ -1,5 +1,6 @@
 import {inject} from 'aurelia-framework';
 import {Api} from '~/services/api';
+import {notify} from '~/services/notification';
 
 @inject(Api)
 export class BatchList {
@@ -14,8 +15,29 @@ export class BatchList {
     this.api = api;
   }
 
+  async saveTrackingInfo(batch) {
+    // save status on orders in batches
+    try {
+      this.saving = true;
+      const requests = (await this.api.fetch('requests', {filter: {'batch_id:eq': batch.id }, page: {size: 100}})).results;
+      requests.forEach(async (request) => {
+        await this.api.edit(`requests/${request.id}`, {status: 'shipping'});
+      });
+      // create aftership tracking
+      await this.api.create('integrations/aftership', {tracking_number: batch.temp_tracking_code, order_id: batch.id, title: batch.name});
+      await this.api.edit(`batches/${batch.id}`, {tracking_code: batch.temp_tracking_code});
+      batch.tracking_code = batch.temp_tracking_code;
+      this.saving = false;
+      notify().log('Successfully saved tracking info');
+    } catch (e) {
+      this.saving = false;
+      console.log(e);
+    }
+  }
+
   async activate(params) {
     this.params = params;
+    this.batches.params.filter['shop_id:eq'] = Number(params.shop_id);
     try {
       const batchesData = await this.api.fetch('batches', this.batches.params);
       this.batches.data = batchesData.results;
