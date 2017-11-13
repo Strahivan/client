8122e684fac15010df8e331ad2f4abc85637093a
AFM Sayem <afm.sayem@gmail.com>
2017-11-13 16:56:24 +0800

    feat(live shopping): add page for live shopping

--- src/views/promotions/index.js
+++ src/views/promotions/index.js
@@ -2,7 +2,8 @@ export class PromotionsRouter {
   configureRouter(config, router) {
     config.map([
       {route: '', name: 'all', moduleId: './fila-giveaway'},
-      {route: 'novelship-giveaway', name: 'fila-giveaway', auth: true, moduleId: './fila-giveaway'}
+      {route: 'novelship-giveaway', name: 'fila-giveaway', auth: true, moduleId: './fila-giveaway'},
+      {route: 'liveshopping', name: 'liveshopping', moduleId: './liveshopping'}
     ]);
   }
 }
---
+++ src/views/promotions/liveshopping.js
@@ -0,0 +1,45 @@
+export class LiveShopping {
+  constructor() {
+    this.quantity = 1;
+  }
+  attached() {
+    paypal.Button.render({
+
+      env: 'production', // sandbox | production
+
+      // PayPal Client IDs - replace with your own
+      // Create a PayPal app: https://developer.paypal.com/developer/applications/create
+      client: {
+        sandbox: 'AVC69GGZGSjeiSnJK5REr_bVsCjHngU2eDFegfRmP3K0L_bEjuI3Ah1VbbtSg6M6IYylFMg1-QN_u4Oe',
+        production: 'AQiTpqmI9uXrAZJItFue1ZSzWKl2A3HJnq_5S8qz6UBDFo58qfFdVoiAjE55qS5SLF_mI7-ZUrQmNAKS'
+      },
+
+      // Show the buyer a 'Pay Now' button in the checkout flow
+      commit: true,
+
+      // payment() is called when the button is clicked
+      payment: (data, actions) => {
+        // Make a call to the REST api to create the payment
+        this.price = (Number(this.price)).toFixed(2);
+        return actions.payment.create({
+          payment: {
+            transactions: [
+              {
+                amount: { total: this.price, currency: 'SGD' }
+              }
+            ]
+          }
+        });
+      },
+
+      // onAuthorize() is called when the buyer approves the payment
+      onAuthorize: (data, actions) => {
+        // Make a call to the REST api to execute the payment
+        return actions.payment.execute().then(function() {
+          window.alert('Payment Complete!');
+        });
+      }
+
+    }, '#paypal-button-container');
+  }
+}
