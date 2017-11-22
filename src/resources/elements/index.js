4aee3b94696d098d34636ed182f0760e965e96c3
AFM Sayem <afm.sayem@gmail.com>
2017-11-22 12:45:04 +0800

    refactor(cleanup): remove unused elements

--- src/resources/elements/index.js
+++ src/resources/elements/index.js
@@ -1,23 +1,17 @@
 const elements = [
-  'animated-tag/animated-tag',
   'bank-payment/bank-payment',
   'carousel/carousel',
-  'card-payment/card-payment',
   'editable-address/editable-address',
   'editable-selection/editable-selection',
   'editable-text/editable-text',
-  'gallery/gallery',
   'hero-carousel/hero-carousel',
-  'hotjar/hotjar',
   'intercom/intercom',
   'loading/loading',
   'navbar/navbar',
   'pagination/pagination',
   'pagination-ajax/pagination-ajax',
-  'payment/payment',
   'product-card/product-card',
-  'searchbar/searchbar',
-  'shipping-estimator/shipping-estimator'
+  'searchbar/searchbar'
 ];
 
 export default elements.map(elem => `./elements/${elem}`);
