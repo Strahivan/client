e02893f4c492afa19a10224ee502350bbda55945
AFM Sayem <afm.sayem@gmail.com>
2017-11-22 17:52:53 +0800

    fix(paypal cancel): handle order cancellation properly

--- src/views/user/requests/canceled.js
+++ src/views/user/requests/canceled.js
@@ -1,21 +1,23 @@
 import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
 import {Api} from '~/services/api';
+import {ErrorReporting} from '~/services/error-reporting';
 
-@inject(Api, Router)
+@inject(Api, Router, ErrorReporting)
 export class Canceled {
-  constructor(api, router) {
+  constructor(api, router, errorReporting) {
     this.api = api;
     this.router = router;
+    this.errorReporting = errorReporting;
   }
 
   activate(params) {
-    this.api.edit(`me/requests/${params.request_id}`, {status: 'canceled', active: false})
+    this.api.edit(`me/requests/${params.request_id}`, {status: 'paypal_canceled'})
       .then(result => console.log(result))
-      .catch(err => console.log(err));
+      .catch(err => this.errorReporting.notifyAndReport(err, 'We were unable to process the request. Please contact support: support@novelship.com'));
 
     this.api.fetch(`me/requests/${params.request_id}`)
       .then((request) => setTimeout(() => window.location.href = `products/${request.product_id}`, 3000))
-      .catch(err => console.log(err));
+      .catch(err => this.errorReporting.notifyAndReport(err, 'We were unable to process the request. Please contact support: support@novelship.com'));
   }
 }
