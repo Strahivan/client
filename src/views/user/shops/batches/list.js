8f74e887b3ea9d0a1cad111a7bde7a1cf9580524
AFM Sayem <afm.sayem@gmail.com>
2017-11-24 21:16:23 +0800

    fix(batch): no longer using async await syntax in frontend

--- src/views/user/shops/batches/list.js
+++ src/views/user/shops/batches/list.js
@@ -15,35 +15,44 @@ export class BatchList {
     this.api = api;
   }
 
-  async saveTrackingInfo(batch) {
+  saveTrackingInfo(batch) {
     // save status on orders in batches
-    try {
-      this.saving = true;
-      const requests = (await this.api.fetch('requests', {filter: {'batch_id:eq': batch.id }, page: {size: 100}})).results;
-      requests.forEach(async (request) => {
-        await this.api.edit(`requests/${request.id}`, {status: 'shipping'});
-      });
-      // create aftership tracking
-      await this.api.create('integrations/aftership', {tracking_number: batch.temp_tracking_code, order_id: batch.id, title: batch.name});
-      await this.api.edit(`batches/${batch.id}`, {tracking_code: batch.temp_tracking_code});
-      batch.tracking_code = batch.temp_tracking_code;
-      this.saving = false;
-      notify().log('Successfully saved tracking info');
-    } catch (e) {
-      this.saving = false;
-      console.log(e);
-    }
+    this.saving = true;
+    this.api
+      .fetch('requests', {filter: {'batch_id:eq': batch.id }, page: {size: 100}})
+      .then(requests => {
+        const requestList = requests.map(request => this.api.edit(`requests/${request.id}`, {status: 'shipping'}));
+        const requestListWithAftership = requestList.concat(this.api
+          .create('integrations/aftership', {
+            tracking_number: batch.temp_tracking_code,
+            order_id: batch.id,
+            order_id_path: `${window.location.href.split('batches')[0]}requests/list?batch_id=${batch.id}`,
+            title: batch.name,
+            custom_fields: {
+              shop_id: this.params.shop_id
+            }
+          }));
+        return Promise.all(requestListWithAftership);
+      })
+      .then(success => this.api.edit(`batches/${batch.id}`, {tracking_code: batch.temp_tracking_code}))
+      .then(success => {
+        batch.tracking_code = batch.temp_tracking_code;
+        this.saving = false;
+        notify().log('Successfully saved tracking info');
+      })
+    .catch(e => notify().log('Error: could not save tracking code.'));
   }
 
-  async activate(params) {
+  activate(params) {
     this.params = params;
     this.batches.params.filter['shop_id:eq'] = Number(params.shop_id);
-    try {
-      const batchesData = await this.api.fetch('batches', this.batches.params);
-      this.batches.data = batchesData.results;
-    } catch (e) {
-      console.log(e);
-    }
+    this.api.fetch('batches', this.batches.params)
+      .then(batchesData => {
+        this.batches.data = batchesData.results;
+      })
+      .catch(error => {
+        notify().log('Could not load the batches');
+      });
   }
 
 }
