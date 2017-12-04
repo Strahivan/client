e173984036b0723fb90028e91d10543aacde333d
AFM Sayem <afm.sayem@gmail.com>
2017-12-04 14:49:04 +0800

    feat(referral code): reset wallet after purchase with virtual credit

--- src/views/user/requests/acknowledge.js
+++ src/views/user/requests/acknowledge.js
@@ -2,32 +2,75 @@ import {inject} from 'aurelia-framework';
 import {Api} from '~/services/api';
 import {HttpClient, json} from 'aurelia-fetch-client';
 import {ErrorHandler} from '~/services/error';
+import {UserStore} from '~/stores/user';
+import {constants} from '~/services/constants';
 
-@inject(Api, HttpClient, ErrorHandler)
+@inject(Api, HttpClient, ErrorHandler, UserStore)
 export class Acknowledge {
-  constructor(api, http, errorHandler) {
+  constructor(api, http, errorHandler, userStore) {
     this.api = api;
     this.http = http;
     this.errorHandler = errorHandler;
+    this.userStore = userStore;
+  }
+
+  updateReferralCode(requestId) {
+    // TODO: Move these calls to server side
+    this.http
+      .fetch(`me/requests/${requestId}`)
+      .then(res => res.json())
+      .then(request => {
+        if (request.referred_by) {
+          return this.api.fetch(`users/${request.customer_id}`);
+        }
+        return Promise.resolve();
+      })
+      .then(user => {
+        if (user) {
+          return Promise.all([
+            this.api.edit('me', {referred_by: user.id}),
+            this.api.edit(`users/${user.id}`, {referral_credit: user.referral_credit + constants.referralCredit})
+          ]);
+        }
+      })
+      .catch(this.errorHandler.notifyAndReport);
+  }
+
+  updateVirtualCredit(requestId) {
+    // update virtual credit and remove the difference from the wallet
+    this.http
+      .fetch(`me/requests/${requestId}`)
+      .then(res => res.json())
+      .then(request => {
+        if (request.applied_credit) {
+          this.userStore.user.referral_credit = 0;
+          return this.api.edit('me', {referral_credit: 0});
+        }
+      })
+      .catch(this.errorHandler.notifyAndReport);
   }
 
   activate(params) {
+    this.updateReferralCode(params.request_id);
+    this.updateVirtualCredit(params.request_id);
     if (params.paymentId && params.PayerID) {
       this.loading = true;
       this.http
       .fetch(`integrations/paypal/execute?payer_id=${params.PayerID}&payment_id=${params.paymentId}`)
       .then(response => response.json())
       .then((approved) => {
-        // if authorization then preorder
-        const updates = {};
+        const updates = {
+          status: 'confirmed',
+          payment_method: 'paypal',
+          payment_id: approved.id,
+          active: true
+        };
+
         const authorization = approved.transactions[0].related_resources[0].authorization;
         if (authorization) {
           updates.authorization_id = authorization.id;
         }
-        updates.status = 'confirmed';
-        updates.payment_method = 'paypal';
-        updates.payment_id = approved.id;
-        updates.active = true;
+
         return this.http
           .fetch(`me/requests/${params.request_id}`, {
             method: 'PUT',
